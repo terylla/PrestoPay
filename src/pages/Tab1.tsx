b37209737daecf30fc7c475d9091f763b39130e7
Ethan <ethanprjohnson@gmail.com>
2020-11-26 20:43:39 -0500

    init

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, {useState} from 'react';
+import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import {NFC} from "@ionic-native/nfc";
+
 
 const Tab1: React.FC = () => {
+    const [values, setvalues] = useState('waiting to scan');
+
+    function startScan(){
+        NFC.scanTag().then(
+            tag => {
+                console.log(JSON.stringify(tag))
+                if (tag.id) {
+                    let test = NFC.bytesToHexString(tag.id);
+                    setvalues(test)
+                }
+            },
+            err => console.log(err)
+        );
+    }
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +34,8 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+          <IonButton onClick={() => startScan()}> click here to scan</IonButton>
+          <p>{values}</p>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
